63b565caa012a3f92ac47ba078d5676acdf6a28a
chinara <chinara.isabaeva@gmail.com>
2019-02-17 12:03:28 +0000

    add .gitignore rules

--- api.py
+++ api.py
@@ -8,6 +8,11 @@ app = Flask(__name__)
 api = Api(app)
 
 
+@app.route('/')
+def index():
+    return "Server started"
+
+
 class Nested(Resource):
     def post(self):
         if not request.args.get('keys'):
@@ -18,8 +23,8 @@ class Nested(Resource):
             return output
 
 
-api.add_resource(Nested, '/')
+api.add_resource(Nested, '/api')
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
